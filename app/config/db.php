ce77f5ee9ee5f2c93121c97e3983b837b55d203c
Savvanis Spyros <savvaniss@yahoo.gr>
2016-11-14 08:02:12 +0200

    Update db.php

--- app/config/db.php
+++ app/config/db.php
@@ -1,13 +1,13 @@
 <?php
 return [
     'mysql'=>[
-        'driver' => 'mysql',
-        'host' => 'localhost',
-        'database' => 'test',
-        'username' => 'test',
-        'password' => 'lucnuc32801',
-        'charset' => 'utf8',
-        'collation' => 'utf8_unicode_ci',
+        'driver' => '',
+        'host' => '',
+        'database' => '',
+        'username' => '',
+        'password' => '',
+        'charset' => '',
+        'collation' => '',
         'prefix' => ''
     ]
 ];

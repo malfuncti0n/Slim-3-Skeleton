91b5c77f002afb82d2211b52ac2401763f0c4683
loukovits <loukovits@hotmail.com>
2016-11-16 19:28:05 +0200

    done

--- app/config/db.php
+++ app/config/db.php
@@ -4,7 +4,7 @@ return [
         'driver' => 'mysql',
         'host' => 'localhost',
         'database' => 'test',
-        'username' => 'root',
+
         'password' => 'lucnuc32801',
         'charset' => 'utf8',
         'collation' => 'utf8_unicode_ci',

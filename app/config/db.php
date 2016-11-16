01cb833b93dbef442c2c028a8f97e7283e2bfa19
loukovits <loukovits@hotmail.com>
2016-11-16 20:03:41 +0200

    db fix

--- app/config/db.php
+++ app/config/db.php
@@ -1,11 +1,11 @@
 <?php
 return [
     'mysql'=>[
-        'driver' => 'mysql',
-        'host' => 'localhost',
-        'database' => 'test',
-        'username' => 'root',
-        'password' => 'lucnuc32801',
+        'driver' => '',
+        'host' => '',
+        'database' => '',
+        'username' => '',
+        'password' => '',
         'charset' => 'utf8',
         'collation' => 'utf8_unicode_ci',
         'prefix' => ''

edff1da2fd7a10e61a22c5a37759f7f2c3c2b9a0
Savvanis Spyros <savvaniss@yahoo.gr>
2016-11-19 12:38:42 +0200

    fix

--- app/routes.php
+++ app/routes.php
@@ -21,7 +21,6 @@ $app->group('', function (){
 
     $this->get('/auth/password/change', 'PasswordController:getChangePassword')->setName('auth.password.change');
     $this->post('/auth/password/change', 'PasswordController:postChangePassword');
-    $this->get('/video/upload', 'VideoController:getVideoUpload')->setName('video.upload');
 
     $this->get('/admin/users', 'UsersController:index')->setName('admin.users');
     $this->delete('/admin/users', 'UsersController:deleteUser')->setName('admin.users.delete');

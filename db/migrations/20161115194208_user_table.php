8d1f9efc52b2a1561e7c023105476780f8df4d21
Savvanis Spyros <savvaniss@yahoo.gr>
2016-11-19 12:33:14 +0200

    implement migrations

--- db/migrations/20161115194208_user_table.php
+++ db/migrations/20161115194208_user_table.php
@@ -25,8 +25,25 @@ class UserTable extends AbstractMigration
      * Remember to call "create()" or "update()" and NOT "save()" when working
      * with the Table class.
      */
-    public function change()
+    public function up()
     {
+        $users=$this->table('users');
+        $users->addColumn('name', 'string', array('limit' => 50))
+              ->addColumn('email', 'string', array('limit' => 50))
+              ->addColumn('username', 'string', array('limit' => 50,'null' =>true))
+              ->addColumn('token', 'string', array('limit' => 255,'null' =>true))
+              ->addColumn('verified', 'binary', array('default' => 0))
+              ->addColumn('password', 'string', array('limit' => 255))
+              ->addColumn('created_at', 'daytime', array('limit' => 255))
+              ->addColumn('created_at', 'datetime')
+              ->addColumn('updated', 'datetime', array('null' => true))
+              ->addIndex(array('email'), array('unique' => true))
+              ->save();
 
+
+    }
+
+    public function down(){
+        $this->dropTable('users');
     }
 }

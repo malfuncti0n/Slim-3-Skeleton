732c23a99220851f9f933736f22a7c522cd93c81
Savvanis Spyros <savvaniss@yahoo.gr>
2016-11-26 11:05:13 +0200

    use of Ternary Operator

--- app/Middleware/RouteMiddleware.php
+++ app/Middleware/RouteMiddleware.php
@@ -9,20 +9,16 @@ class RouteMiddleware extends Middleware
 {
     public function __invoke($request, $response, $next)
     {
-        //with zero we count guest
-        if(!$this->container->auth->check()){
-            $userid=0;
-        }else{
-            $userid=$this->container->auth->user()->id;
-        }
-        //get details from route
+         //with zero we count guest
+         $userid=(!$this->container->auth->check() ? 0 : $this->container->auth->user()->id);
+         //get details from route
          $route=$request->getAttribute('route');
          $audit=new Audit;
          $audit->route=$route->getName();
          $method=$route->getMethods();
          $audit->method=$method[0];
          $audit->userid=$userid;
-        //save the request to database
+         //save the request to database
          $audit->save();
 
         //call next middleware
